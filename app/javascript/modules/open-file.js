49c8420a9a792c13d3a3c095fe88b481ccd36fea
Anatoli Makarevich <makaroni4@gmail.com>
2021-10-03 13:39:52 +0200

    Open file in VSCode

---
+++ app/javascript/modules/open-file.js
@@ -0,0 +1,15 @@
+const openFileInVSCode = (projectPath, filePath) => {
+  window.location = `vscode://file${projectPath}`;
+  setTimeout(() => {
+    window.location = `vscode://file${filePath}`;
+  }, 1000);
+};
+
+document.addEventListener("click", e => {
+  if(e.target.matches(".js-open-file")) {
+    const projectPath = e.target.dataset.projectPath;
+    const filePath = e.target.dataset.filePath;
+
+    openFileInVSCode(projectPath, filePath);
+  }
+});
